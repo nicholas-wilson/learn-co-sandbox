ccab131572fcd8f1f2c93123c69e86af3a0711f3
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-10-05 20:51:30 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,11 @@
+# this is a comment not // 
+dog = "brownie"
+dog_counter = 0
+
+while (dog == "brownie") do
+  if (dog_counter == 3)
+    break
+  end
+  puts "who's a good boy"
+  dog_counter += 1
+end
